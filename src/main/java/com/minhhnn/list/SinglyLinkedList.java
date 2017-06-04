c4b048965d17a54678e4343ede55b79a0d3002e2
Minh Huynh <minhhnn@gmail.com>
2017-06-04 13:18:37 +1000

    Add method delete, toArray to singlylinkedlist

--- src/main/java/com/minhhnn/list/SinglyLinkedList.java
+++ src/main/java/com/minhhnn/list/SinglyLinkedList.java
@@ -1,15 +1,25 @@
 package com.minhhnn.list;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by minh on 3/06/17.
  */
-public class SinglyLinkedList<T> {
+public class SinglyLinkedList<T>{
+
     private class Node<T> {
         T data;
         Node next;
 
         Node(T data) {
             this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    '}';
         }
 
         @Override
@@ -61,6 +71,56 @@ public class SinglyLinkedList<T> {
             //Increase list size
             size++;
         }
+    }
+
+    /**
+     * Delete an item from the list
+     * @param data item to be deleted
+     */
+    public void delete(T data) {
+        if (head != null) {
+            if (head.data.equals(data)) {
+                head = head.next;
+                size--;
+            }
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.data.equals(data)) {
+                    current.next = current.next.next;
+                    size--;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    /**
+     * Convert a list to an array
+     * @return an array containing all the elements in the list
+     */
+    public T[] toArray() {
+        if (head == null) {
+            return (T[]) new Object[0];
+        }
+        Node current = head;
+        T[] result = createArray(head.data.getClass(), size);
+        int i = 0;
+        while (current != null) {
+            result[i] = (T) current.data;
+            current = current.next;
+            i++;
+        }
+        return (T[]) result;
+    }
+
+    /**
+     * Helper method to create an array of generic type
+     * @param type
+     * @param size
+     * @return
+     */
+    private T[] createArray(Class type, int size){
+        return (T[]) Array.newInstance(type, size);
     }
 
     @Override
--- src/test/java/com/minhhnn/list/SinglyLinkedListTest.java
+++ src/test/java/com/minhhnn/list/SinglyLinkedListTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -22,15 +23,68 @@ public class SinglyLinkedListTest {
         //Generate test list
         List<Integer> testList = generateTestList();
 
-        // Fill the list with n random integers
-        StringBuilder expectedOutput = new StringBuilder("List: ");
+        // Fill our list with n random integers
         for (Integer num : testList) {
-            expectedOutput.append(num).append(" ");
             list.insert(num);
         }
 
         Assert.assertThat("Size of list must equal to n", list.size(), CoreMatchers.equalTo(testList.size()));
-        Assert.assertThat("List output must equal to expectedOutput", list.toString(), CoreMatchers.equalTo(expectedOutput.toString()));
+        Assert.assertThat("List data must equal to testList", compartTwoArrays(list.toArray(), testList.toArray()), CoreMatchers.equalTo(true));
+    }
+
+    @Test
+    public void testDeleteHead() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Assert.assertThat("List is empty", list.size(), CoreMatchers.equalTo(0));
+
+        //Generate test list
+        List<Integer> testList = generateTestList();
+
+        // Fill our list with n random integers
+        for (Integer num : testList) {
+            list.insert(num);
+        }
+
+        //Delete the head element from the testList
+        //Element to be deleted
+        Integer data = testList.get(0);
+
+        testList.remove(0);
+
+        //Delete the head element from our list
+        list.delete(data);
+
+        Assert.assertThat("Size of list must equal to size of test list", list.size() , CoreMatchers.equalTo(testList.size()));
+        Assert.assertThat("List data must equal to testList", Arrays.equals(list.toArray(), testList.toArray()), CoreMatchers.equalTo(true));
+
+    }
+
+    @Test
+    public void testDeleteARandomIndex() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Assert.assertThat("List is empty", list.size(), CoreMatchers.equalTo(0));
+
+        //Generate test list
+        List<Integer> testList = generateTestList();
+
+        // Fill our list with n random integers
+        for (Integer num : testList) {
+            list.insert(num);
+        }
+
+        //Pick a random element to delete
+        int indexOfElementToBeDeleted = randomGenerator.nextInt(testList.size());
+        System.out.println("index = " + indexOfElementToBeDeleted);
+
+        //Delete an element from the testList
+        Integer data = testList.get(indexOfElementToBeDeleted);
+        testList.remove(data);
+
+        //Delete an element from our list
+        list.delete(data);
+
+        Assert.assertThat("Size of list must equal to size of test list", list.size() , CoreMatchers.equalTo(testList.size()));
+        Assert.assertThat("List data must equal to testList", Arrays.equals(list.toArray(), testList.toArray()), CoreMatchers.equalTo(true));
 
     }
 
@@ -52,6 +106,26 @@ public class SinglyLinkedListTest {
         }
 
         return testList;
+    }
+
+    /**
+     * Helper method to compare 2 arrays
+     * @param arr1
+     * @param arr2
+     * @return true if 2 arrays are equals, and false otherwise
+     */
+    private boolean compartTwoArrays(Object[] arr1, Object[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i].getClass() != arr2[i].getClass() || !arr1[i].equals(arr2[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     /**
